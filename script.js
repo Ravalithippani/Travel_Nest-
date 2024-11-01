bf51650ca6a751a8faa9885c1da31496aeac5825
Ravalithippani <ravalithippani001@gmail.com>
2024-11-01 11:46:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const goToPage = () => {
+    window.location.href = "beaches.html";
+};
+
+const goToPage3 = () => {
+    window.location.href = "waterfalls.html";
+};
+
+const goToPage2 = () => {
+    window.location.href = "temples.html";
+};
+
+const goToPage1 = () => {
+    window.location.href = "hillstations.html";
+};
